c690bb4c82f616823d30c3fa58ca4aab10794618
Carlos Rocha <carlinhozrocha@gmail.com>
2020-07-04 16:11:49 -0300

    update

--- js/open.js
+++ js/open.js
@@ -1,7 +1,12 @@
-var x = querySelectorAll("caixinha");
 if(screen.width > 480){
    	location.href="desktop.html";
  }
-if(screen.width <= 320){
+/*if(screen.width <= 320){
+	var x = document.getElementById("caixinha");
 	x.style.display = "block";
 }
+else{
+	var y = document.getElementById("caixinha");
+	y.style.display = "none";
+}
+*/
--- js/redirecionar.js
+++ js/redirecionar.js
@@ -1,8 +1,3 @@
 if(screen.width <= 480){
    	location.href="index.html";
  }
-var x = document.getElementById("corp");
-/*if(x <= 480){
-	location.href="index.html";
-}
-/*
