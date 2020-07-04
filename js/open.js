13f382bcd4eb170a20b84b64856d1cc92052223c
Carlos Rocha <carlinhozrocha@gmail.com>
2020-07-04 16:21:04 -0300

    Update open.js

--- js/open.js
+++ js/open.js
@@ -1,12 +1,3 @@
 if(screen.width > 480){
    	location.href="desktop.html";
  }
-/*if(screen.width <= 320){
-	var x = document.getElementById("caixinha");
-	x.style.display = "block";
-}
-else{
-	var y = document.getElementById("caixinha");
-	y.style.display = "none";
-}
-*/
